f2c733f60affe63fa1c0f0d8fbf5a676dfc6f849
syed745 <syedb1997@gmail.com>
2024-02-21 23:56:13 +0500

    working_ui

---
+++ lib/bloc/weather_bloc_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'weather_bloc_event.dart';
+part 'weather_bloc_state.dart';
+
+class WeatherBlocBloc extends Bloc<WeatherBlocEvent, WeatherBlocState> {
+  WeatherBlocBloc() : super(WeatherBlocInitial()) {
+    on<WeatherBlocEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
